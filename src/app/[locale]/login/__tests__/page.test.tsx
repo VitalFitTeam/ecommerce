6a148b293ed6ffe9a3c8954602326ce6b764096d
AlbaniBarragan <155846628+albanibarragan@users.noreply.github.com>
2025-11-02 09:44:23 -0400

    fix: error en los test del login

--- src/app/[locale]/login/__tests__/page.test.tsx
+++ src/app/[locale]/login/__tests__/page.test.tsx
@@ -1,33 +1,46 @@
 import { render, screen } from "@testing-library/react";
 import LoginPage from "../page";
+import React from "react";
 
-// Mock de next/router
-jest.mock("next/navigation", () => ({
-  useRouter() {
-    return {
-      prefetch: () => null,
-    };
-  },
+jest.mock("next-intl", () => ({
+  useTranslations: () => (key: string) => key, // Devuelve la clave (ej. "LoginPage.title")
+  useLocale: () => "es",
 }));
+
+jest.mock("@/i18n/routing", () => {
+  // Creamos el componente 'Link' simulado por separado
+  const MockedLink = React.forwardRef(({ href, ...props }: any, ref) => (
+    <a ref={ref} href={href} {...props} />
+  ));
+  MockedLink.displayName = "MockedLink";
+
+  return {
+    useRouter: jest.fn(() => ({
+      replace: jest.fn(),
+      push: jest.fn(),
+    })),
+    usePathname: jest.fn(() => "/login"),
+    Link: MockedLink,
+  };
+});
 
 describe("Login Page", () => {
   it("should render the login form", () => {
     render(<LoginPage />);
-
-    // Check for the heading
-    const heading = screen.getByRole("heading", { name: /iniciar sesión/i });
+    const heading = screen.getByRole("heading", { name: "LoginPage.title" });
     expect(heading).toBeInTheDocument();
-
-    // Check for input fields
-    const emailInput = screen.getByPlaceholderText(/email/i);
+    const emailInput = screen.getByPlaceholderText(
+      "LoginPage.form.emailPlaceholder",
+    );
     expect(emailInput).toBeInTheDocument();
 
-    const passwordInput = screen.getByPlaceholderText(/ingresa tu contraseña/i);
+    const passwordInput = screen.getByPlaceholderText(
+      "LoginPage.form.passwordPlaceholder",
+    );
     expect(passwordInput).toBeInTheDocument();
 
-    // Check for the submit button
     const submitButton = screen.getByRole("button", {
-      name: /^iniciar sesión$/i,
+      name: "LoginPage.form.submitButton",
     });
     expect(submitButton).toBeInTheDocument();
   });
