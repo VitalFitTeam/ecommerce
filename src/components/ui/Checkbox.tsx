612ad2e41ea1c5ce4a9a8ca39c2ff6fe7b27751f
AlbaniBarragan <155846628+albanibarragan@users.noreply.github.com>
2025-11-13 00:17:13 -0400

    feat: componente tabla con shadcn y paginacion

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,30 +1,30 @@
-import React from "react";
+"use client";
 
-type CheckboxProps = {
-  labelText: string;
-  isChecked: boolean;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-};
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "lucide-react";
 
-export default function Checkbox({
-  labelText,
-  isChecked,
-  onChange,
-}: CheckboxProps) {
-  const id = `checkbox-${labelText.replace(/\s+/g, "-").toLowerCase()}`;
+import { cn } from "@/lib/utils";
 
-  return (
-    <div className="flex items-center">
-      <input
-        id={id}
-        type="checkbox"
-        checked={isChecked}
-        onChange={onChange}
-        className="cursor-pointer"
-      />
-      <label htmlFor={id} className="ml-2 cursor-pointer text-sm">
-        {labelText}
-      </label>
-    </div>
-  );
-}
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      "grid place-content-center peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      className,
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator
+      className={cn("grid place-content-center text-current")}
+    >
+      <Check className="h-4 w-4" />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
